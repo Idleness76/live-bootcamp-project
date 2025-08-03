22ce5d989255bd56e592009af63146457f9e4615
Idleness76 <james.barris@gmail.com>
2025-08-03 10:00:15 -0400

    add (failing) 400 and 401 tests for logout

--- auth-service/tests/api/logout.rs
+++ auth-service/tests/api/logout.rs
@@ -1,8 +1,45 @@
+use auth_service::{utils::JWT_COOKIE_NAME, ErrorResponse};
+use reqwest::Url;
+
 use crate::helpers::TestApp;
 
 #[tokio::test]
-async fn logout_route_works() {
+async fn should_return_400_if_jwt_cookie_missing() {
     let app = TestApp::new().await;
+
     let response = app.post_logout().await;
-    assert_eq!(response.status().as_u16(), 200);
+
+    assert_eq!(response.status(), 400);
+
+    let error_response: ErrorResponse = response
+        .json()
+        .await
+        .expect("Failed to parse response body");
+
+    assert_eq!(error_response.error, "Missing auth token");
+}
+
+#[tokio::test]
+async fn should_return_401_if_invalid_token() {
+    let app = TestApp::new().await;
+
+    // add invalid cookie
+    app.cookie_jar.add_cookie_str(
+        &format!(
+            "{}=invalid; HttpOnly; SameSite=Lax; Secure; Path=/",
+            JWT_COOKIE_NAME
+        ),
+        &Url::parse("http://127.0.0.1").expect("Failed to parse URL"),
+    );
+
+    let response = app.post_logout().await;
+
+    assert_eq!(response.status(), 401);
+
+    let error_response: ErrorResponse = response
+        .json()
+        .await
+        .expect("Failed to parse response body");
+
+    assert_eq!(error_response.error, "Invalid token");
 }
