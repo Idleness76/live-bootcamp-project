4350d0455e8a161783b002f70aba422a0a1293e2
Idleness76 <james.barris@gmail.com>
2025-08-04 10:23:01 -0400

    update logout route to add token to banned token store

--- auth-service/src/routes/logout.rs
+++ auth-service/src/routes/logout.rs
@@ -1,22 +1,37 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{extract::State, http::StatusCode, response::IntoResponse};
 use axum_extra::extract::CookieJar;
 
 use crate::{
+    app_state::AppState,
     domain::AuthAPIError,
     utils::{validate_token, JWT_COOKIE_NAME},
 };
 
-pub async fn logout(jar: CookieJar) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
-    // Check if the JWT cookie exists in the jar.
+pub async fn logout(
+    State(app_state): State<AppState>,
+    jar: CookieJar,
+) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
     let cookie = match jar.get(JWT_COOKIE_NAME) {
         Some(cookie) => cookie,
         None => return (jar, Err(AuthAPIError::MissingToken)),
     };
 
-    let token = cookie.value().to_owned();
+    let token = cookie.value();
 
-    match validate_token(&token).await {
-        Ok(_claims) => (jar.remove(JWT_COOKIE_NAME), Ok(StatusCode::OK)),
-        Err(_) => (jar, Err(AuthAPIError::InvalidToken)),
+    if validate_token(&token).await.is_err() {
+        return (jar, Err(AuthAPIError::InvalidToken));
     }
+
+    if app_state
+        .banned_token_store
+        .write()
+        .await
+        .add_banned_token(token)
+        .await
+        .is_err()
+    {
+        return (jar, Err(AuthAPIError::UnexpectedError));
+    }
+
+    (jar.remove(JWT_COOKIE_NAME), Ok(StatusCode::OK))
 }
