2b59bd327a6c74e17b7fd3f72e4fc62dc46d8ec7
Idleness76 <james.barris@gmail.com>
2025-08-10 11:51:15 -0400

    update 2fa handler to properly serialize request and update nginx config for 2fa route

--- auth-service/src/routes/verify_2fa.rs
+++ auth-service/src/routes/verify_2fa.rs
@@ -6,52 +6,47 @@ use axum::{http::StatusCode, response::IntoResponse, Json};
 use axum_extra::extract::CookieJar;
 use serde::Deserialize;
 
-#[axum::debug_handler]
 pub async fn verify_2fa(
     State(state): State<AppState>,
     jar: CookieJar,
     Json(request): Json<Verify2FARequest>,
-) -> (CookieJar, Result<axum::response::Response, AuthAPIError>) {
-    let email = match Email::parse(request.email) {
-        Ok(email) => email,
-        Err(_) => return (jar, Err(AuthAPIError::IncorrectCredentials)),
-    };
+) -> Result<(CookieJar, impl IntoResponse), AuthAPIError> {
+    let email = Email::parse(request.email).map_err(|_| AuthAPIError::IncorrectCredentials)?;
 
-    let (stored_login_attempt_id, stored_two_fa_code) =
-        match state.two_fa_code_store.read().await.get_code(&email).await {
-            Ok(codes) => codes,
-            Err(_) => return (jar, Err(AuthAPIError::IncorrectCredentials)),
-        };
+    let (stored_login_attempt_id, stored_two_fa_code) = state
+        .two_fa_code_store
+        .read()
+        .await
+        .get_code(&email)
+        .await
+        .map_err(|_| AuthAPIError::IncorrectCredentials)?;
 
     if stored_login_attempt_id.as_ref() != request.login_attempt_id
         || stored_two_fa_code.as_ref() != request.two_fa_code
     {
-        return (jar, Err(AuthAPIError::IncorrectCredentials));
+        return Err(AuthAPIError::IncorrectCredentials);
     }
 
-    match state
+    state
         .two_fa_code_store
         .write()
         .await
         .remove_code(&email)
         .await
-    {
-        Ok(()) => (),
-        Err(_) => return (jar, Err(AuthAPIError::UnexpectedError)),
-    };
+        .map_err(|_| AuthAPIError::UnexpectedError)?;
 
-    let auth_cookie = match generate_auth_cookie(&email) {
-        Ok(cookie) => cookie,
-        Err(_) => return (jar, Err(AuthAPIError::UnexpectedError)),
-    };
+    let auth_cookie = generate_auth_cookie(&email).map_err(|_| AuthAPIError::UnexpectedError)?;
+
     let updated_jar = jar.add(auth_cookie);
 
-    (updated_jar, Ok(StatusCode::OK.into_response()))
+    Ok((updated_jar, StatusCode::OK.into_response()))
 }
 
 #[derive(Deserialize)]
 pub struct Verify2FARequest {
     pub email: String,
+    #[serde(rename = "loginAttemptId")]
     pub login_attempt_id: String,
+    #[serde(rename = "2FACode")]
     pub two_fa_code: String,
 }
