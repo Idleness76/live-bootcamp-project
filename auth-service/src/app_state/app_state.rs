9f2341443e558b05b9baac1fa49d8d9008dad012
Idleness76 <james.barris@gmail.com>
2025-08-10 08:15:47 -0400

    update application state to carry email_client

--- auth-service/src/app_state/app_state.rs
+++ auth-service/src/app_state/app_state.rs
@@ -1,4 +1,4 @@
-use crate::domain::{BannedTokenStore, TwoFACodeStore, UserStore};
+use crate::domain::{BannedTokenStore, EmailClient, TwoFACodeStore, UserStore};
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
@@ -8,6 +8,7 @@ pub struct AppState {
     pub user_store: Arc<RwLock<dyn UserStore + Send + Sync>>,
     pub banned_token_store: Arc<RwLock<dyn BannedTokenStore + Send + Sync>>,
     pub two_fa_code_store: Arc<RwLock<dyn TwoFACodeStore + Send + Sync>>,
+    pub email_client: Arc<dyn EmailClient + Send + Sync>,
 }
 
 impl AppState {
@@ -16,11 +17,13 @@ impl AppState {
         user_store: Arc<RwLock<dyn UserStore + Send + Sync>>,
         banned_token_store: Arc<RwLock<dyn BannedTokenStore + Send + Sync>>,
         two_fa_code_store: Arc<RwLock<dyn TwoFACodeStore + Send + Sync>>,
+        email_client: Arc<dyn EmailClient + Send + Sync>,
     ) -> Self {
         Self {
             user_store,
             banned_token_store,
             two_fa_code_store,
+            email_client,
         }
     }
 }
--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -4,7 +4,7 @@ use tokio::sync::RwLock;
 
 use auth_service::{
     app_state::AppState,
-    services::{HashmapTwoFACodeStore, HashmapUserStore, HashsetBannedTokenStore},
+    services::{HashmapTwoFACodeStore, HashmapUserStore, HashsetBannedTokenStore, MockEmailClient},
     utils::test,
     Application,
 };
@@ -25,10 +25,12 @@ impl TestApp {
         let user_store = Arc::new(RwLock::new(HashmapUserStore::default()));
         let banned_token_store = Arc::new(RwLock::new(HashsetBannedTokenStore::default()));
         let two_fa_code_store = Arc::new(RwLock::new(HashmapTwoFACodeStore::default()));
+        let email_client = Arc::new(MockEmailClient);
         let app_state = AppState::new(
             user_store.clone(),
             banned_token_store.clone(),
             two_fa_code_store.clone(),
+            email_client,
         );
 
         // Build application on random port for test isolation
