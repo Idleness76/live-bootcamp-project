61a6a2485e8050730d58c93524991474cd628b5e
Idleness76 <james.barris@gmail.com>
2025-08-16 13:38:45 -0400

    setup main and test helper to use postgres user store

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,7 +1,9 @@
 use auth_service::{
     app_state::AppState,
     get_postgres_pool,
-    services::{HashmapTwoFACodeStore, HashmapUserStore, HashsetBannedTokenStore, MockEmailClient},
+    services::{
+        HashmapTwoFACodeStore, HashsetBannedTokenStore, MockEmailClient, PostgresUserStore,
+    },
     utils::{prod, DATABASE_URL},
     Application,
 };
@@ -11,17 +13,18 @@ use tokio::sync::RwLock;
 
 #[tokio::main]
 async fn main() {
-    let user_store = Arc::new(RwLock::new(HashmapUserStore::default()));
+    let pg_pool = configure_postgresql().await;
+    let user_store = Arc::new(RwLock::new(PostgresUserStore::new(pg_pool)));
     let banned_token_store = Arc::new(RwLock::new(HashsetBannedTokenStore::default()));
     let two_fa_code_store = Arc::new(RwLock::new(HashmapTwoFACodeStore::default()));
     let email_client = Arc::new(MockEmailClient);
+
     let app_state = AppState::new(
         user_store,
         banned_token_store,
         two_fa_code_store,
         email_client,
     );
-    let pg_pool = configure_postgresql().await;
 
     let app = Application::build(app_state, prod::APP_ADDRESS)
         .await
